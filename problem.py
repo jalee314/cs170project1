486132d03ee1192e6b6252f25137774c312c5531
jalee314 <jasonklee1003@gmail.com>
2024-10-30 23:47:00 -0700

    problem class constructor, goal check method, moving pieces of puzzle method added

--- problem.py
+++ problem.py
@@ -1,4 +1,24 @@
 #headers
 
-class Program:
+'''
+Problem Class:
+Initial State: Parameter that accepts 2D matrix of whatever initial state your feeble mind desires
+Goal State is always just gonna be the puzzle in order
+There are only 4 possible operators here
+'''
+
+
+class Problem:
+    def __init__(self, initial_state):
+        self.initial_state = initial_state
+        self.goal_state = [[1, 2, 3],
+                           [4, 5, 6],
+                           [7, 8, 0]] #star in this context represented by 0.
+        self.operators = ["UP", "DOWN", "LEFT", "RIGHT"]
     
+    def check_goal(self, state):
+        return state == self.goal_state
+    
+    def move(self, state, operator):
+        new_state = state[:]
+        star_index = state.index(0)
