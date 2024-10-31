2007dfcf0de001e2899e13d579769ec0d45f93d6
jalee314 <jasonklee1003@gmail.com>
2024-10-30 22:51:56 -0700

    node class implemented with constructor

---
+++ node.py
@@ -0,0 +1,22 @@
+'''
+Node Class:
+State: Current configuration of the puzzle, represented by 2D matrix
+g: Cost of reaching current node from starting node
+h: Cost of heuristic, estimates cost to get to the goal
+Move: Keeps track of what move was used to get to current state
+parent: Points to the parent node if applicable, will allow me to trace through the final solution path
+
+Default parameters are for starting node, every other node will get those values replaced in code 
+'''
+
+class Node:
+    def __init__(self, state, g, h, move=None, parent=None, depth=0):
+        self.state = state                      
+        self.g = g
+        self.h = h
+        self.move = move
+        self.parent = parent
+        self.depth = depth
+
+
+
